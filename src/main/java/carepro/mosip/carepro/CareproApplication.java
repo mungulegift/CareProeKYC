69eb5fdecded9cbb60e207e971a3db1f94354e97
Gift Mungule <GMungule@SmartCare.org.zm>
2024-06-26 11:15:38 +0200

    Added app files

---
+++ src/main/java/carepro/mosip/carepro/CareproApplication.java
@@ -0,0 +1,13 @@
+package carepro.mosip.carepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/carepro/mosip/carepro/CareproApplicationTests.java
@@ -0,0 +1,13 @@
+package carepro.mosip.carepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
